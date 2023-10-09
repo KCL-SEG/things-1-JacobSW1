a370f8f5517ecb9993c8a0d1706a3c4dfe2248bc
Jacob Whiting <jacob.whiting@kcl.ac.uk>
2023-10-09 12:47:16 +0100

    fix model

--- things/models.py
+++ things/models.py
@@ -1,8 +1,7 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
 from django.core.validators import MaxValueValidator, MinValueValidator
 
-class Thing(AbstractUser):
+class Thing(models.Model):
     name = models.CharField(max_length = 30, unique = True, blank = False)
-    description = models.CharField(max_length = 120, unique = False, blank = False)
+    description = models.CharField(max_length = 120, unique = False, blank = True)
     quantity = models.IntegerField(unique = False, validators=[MinValueValidator(0), MaxValueValidator(100)])
